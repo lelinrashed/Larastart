9009521a8bd8793b85bfb43910b596df7ddc6d9f
lelinrashed <lelin.rashed784@gmail.com>
2019-03-25 18:40:07 +0600

    Push after store user and add vue progress bar and sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -8,6 +7,52 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import moment from 'moment'
+import { Form, HasError, AlertError } from 'vform'
+
+import Swal from 'sweetalert2'
+window.Swal = Swal
+
+const toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
+});
+
+window.toast = toast
+
+window.Form = Form;
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '3px'
+})
+
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default },
+    { path: '/profile', component: require('./components/Profile.vue').default }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes
+})
+
+Vue.filter('upText', function(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+})
+
+Vue.filter('myDate', function(date) {
+    return moment(date).format('MMMM Do YYYY');
+})
 
 /**
  * The following block of code may be used to automatically register your
@@ -29,5 +74,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
---
+++ resources/js/components/Users.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users Table</h3>
+                        <div class="card-tools">
+                            <button data-toggle="modal" data-target="#addNewUser" class="btn btn-primary">Add User &nbsp; <i class="fas fa-user-plus fa-fw"></i> </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Type</th>
+                                    <th>Created at</th>
+                                    <th>Modify</th>
+                                </tr>
+                                <tr v-for="(user,i) in users">
+                                    <td>{{ i+1 }}</td>
+                                    <td>{{ user.name }}</td>
+                                    <td>{{ user.email }}</td>
+                                    <td><span class="tag tag-success">{{ user.type | upText }}</span></td>
+                                    <td>{{ user.created_at | myDate }}</td>
+                                    <td>
+                                        <a href="#">
+                                            <i class="fas fa-edit blue"></i>
+                                        </a>
+                                        <a href="#">
+                                            &nbsp;&nbsp;<i class="fas fa-trash red"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <div class="modal fade" id="addNewUser" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Add New User</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form @submit.prevent="createUser">
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input v-model="form.name" placeholder="Name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
+                                <has-error :form="form" field="name"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <input v-model="form.email" placeholder="Email address" type="email" name="email" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }">
+                                <has-error :form="form" field="email"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <textarea name="bio" v-model="form.bio" placeholder="Short bio for user (Optional)" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"></textarea>
+                                <has-error :form="form" field="bio"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <select name="type" v-model="form.type" class="form-control" :class="{ 'is-invalid' : form.errors.has('type') }">
+                                    <option value="">Select User Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Standard User</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <has-error :form="form" field="type"></has-error>
+                            </div>
+                            <div class="form-group">
+                                <input v-model="form.password" placeholder="Password" type="password" name="password" class="form-control" :class="{ 'is-invalid': form.errors.has('password') }">
+                                <has-error :form="form" field="password"></has-error>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-success">Create User</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            users: {},
+            form: new Form({
+                name: '',
+                email: '',
+                password: '',
+                type: '',
+                bio: '',
+                photo: ''
+            })
+        }
+    },
+    methods: {
+        createUser() {
+            this.$Progress.start()
+            this.form.post('api/user')
+            $('#addNewUser').modal('hide')
+            toast.fire({
+                type: 'success',
+                title: 'User created successfully'
+            })
+            this.$Progress.finish()
+        },
+
+        loadUser() {
+            axios.get('api/user')
+                .then(({ data }) => (this.users = data.data))
+        }
+    },
+    created() {
+        this.loadUser();
+    }
+};
+
+</script>
